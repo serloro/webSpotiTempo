d86d6125fe3227f5ec46c6c460c58c9eb9fa235f
serloro <32571944+serloro@users.noreply.github.com>
2025-07-10 17:59:46 +0200

    Fix Vite base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/spotitempo/',
+  base: '/webSpotiTempo/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
